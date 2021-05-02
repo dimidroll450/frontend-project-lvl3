134db2da7d6057644b78f044335e1231007008b5
dimidroll450 <dmitriykvas@outlook.com>
2021-05-02 15:36:04 +0300

    remove key from parcer.js

--- __tests__/__parser__.js
+++ __tests__/__parser__.js
@@ -7,20 +7,17 @@ const recieved = {
     title: 'Lorem ipsum feed for an interval of 1 minutes with 10 item(s)',
     description: 'This is a constantly updating lorem ipsum feed',
     url: 'http://example.com/',
-    key: 'key',
   },
   posts: [
     {
       title: 'Lorem ipsum 2021-04-27T09:10:00Z',
       description: 'Labore veniam id duis ut eiusmod dolore labore nostrud incididunt excepteur dolore.',
       url: 'http://example.com/test/1619514600',
-      key: 'key',
     },
     {
       title: 'Lorem ipsum 2021-04-27T09:09:00Z',
       description: 'Velit cupidatat ut labore nostrud et qui eu culpa do amet occaecat.',
       url: 'http://example.com/test/1619514540',
-      key: 'key',
     },
   ],
 };
@@ -29,7 +26,7 @@ const getFixturePath = (filename) => path.resolve(process.cwd(), '__tests__/__fi
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
 test('right parse', () => {
-  const actual = parser(readFile('example'), 'key');
+  const actual = parser(readFile('example'));
 
   expect(actual).toMatchObject(recieved);
 });
--- src/parser.js
+++ src/parser.js
@@ -3,23 +3,22 @@ const parseToXml = (data) => {
   return parser.parseFromString(data, 'text/xml');
 };
 
-const generateData = (element, key, parent = '') => {
+const generateData = (element, parent = '') => {
   const result = {
     title: element.querySelector(`${parent} title`).textContent,
     description: element.querySelector(`${parent} description`).textContent,
     url: element.querySelector(`${parent} link`).textContent,
-    key,
   };
   return result;
 };
 
-export default (data, key = '') => {
+export default (data) => {
   const parsed = parseToXml(data);
-  const feed = generateData(parsed, key, 'channel');
+  const feed = generateData(parsed, 'channel');
   const items = parsed.querySelectorAll('item');
   const arrItems = Array.prototype.slice.call(items);
 
-  const posts = arrItems.map((post) => generateData(post, key));
+  const posts = arrItems.map((post) => generateData(post));
 
   return { feed, posts: [...posts] };
 };
